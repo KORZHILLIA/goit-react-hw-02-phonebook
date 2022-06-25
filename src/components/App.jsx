ccaca641c336375e5ead3a6112b08f8ca58ec9f2
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-06-25 18:42:12 +0300

    First effort

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    return this.setState(prevState => {
+      const requiredIdx = prevState.contacts.findIndex(
+        contact => contact.name === newContact.name
+      );
+      if (requiredIdx === -1) {
+        return {
+          contacts: [...prevState.contacts, newContact],
+        };
+      }
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    });
+  };
+
+  filterChangeHandler = value => {
+    return this.setState({ filter: value });
+  };
+
+  deleteClickHandler = id => {
+    const { contacts } = this.state;
+    const requiredContactIdx = contacts.findIndex(contact => contact.id === id);
+    contacts.splice(requiredContactIdx, 1);
+    return this.setState({ contacts });
+  };
+  render() {
+    const { contacts, filter } = this.state;
+    const preparedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(preparedFilter)
+    );
+    return (
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter onChange={this.filterChangeHandler} />
+        <ContactList
+          contacts={filteredContacts}
+          deleteClickHandler={this.deleteClickHandler}
+        />
+      </div>
+    );
+  }
+}
